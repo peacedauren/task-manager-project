4d58cfbf248ba07eae19abe930c7b883b0edf7e5
g1enna210104 <g1enna210104@gmail.com>
2024-08-14 13:06:01 +0500

    Добавил возможность добавления новых колонок

--- src/Entities/Column/Column.tsx
+++ src/Entities/Column/Column.tsx
@@ -33,13 +33,15 @@ type TColumn = {
 }
 
 export const Column = () => {
-    const [columns, setColumns] = useState<TColumn[]>([{column: 'todo', showForm: false}, {column: 'in process', showForm: false}, {column: 'done', showForm: false}]);
+    const [columns, setColumns] = useState<TColumn[]>([]);
     const [tasks, setTasks] = useState<TTask[]>([]);
     const [newTask, setNewTask] = useState<string>('');
     const [currentTask, setCurrentTask] = useState<number>();
     const [showAlert, setShowAlert] = useState(false);
-    const[isModalClosed, setIsModalClosed] = useState(false);
+    const [isModalClosed, setIsModalClosed] = useState(false);
     const [editingTask, setEditingTask] = useState<TTask | null>(null);
+    const [showNewColumn, setShowNewColumn] = useState<boolean>(false);
+    const [newColumn, setNewColumn] = useState<string>();
 
     const onCloseModalHanlder = () => {
         setIsModalClosed(false);
@@ -50,13 +52,28 @@ export const Column = () => {
         setEditingTask(tasks[index]);
     }
 
-    const getTasks = async () => {
+    const getData = async () => {
         const { data } = await axiosTasks.get('tasks.json');
+        const dataColumns = await axiosTasks.get('columns.json');
+
+        if(dataColumns.data) {
+            const takenColumns: TColumn[] = Object.values(dataColumns.data);
+
+            takenColumns.map((takenColumn) => {
+                console.log(takenColumn)
+                if(!columns.find(column => column.column === takenColumn.column)) {
+                    const copyColumns = columns;
+                    copyColumns.push({column: takenColumn.column, showForm: false});
+                    setColumns([...copyColumns]);
+                }
+            })
+        }
+
         if(data) {
             const dataValues: Omit<TTask, 'id'>[] = Object.values(data);
             const dataKeys: string[] = Object.keys(data);
 
-            dataValues.map((value, index) => {
+            dataValues.forEach((value, index) => {
                 const copyTasks = tasks;
                 copyTasks.push({
                     id: dataKeys[index],
@@ -64,14 +81,6 @@ export const Column = () => {
                     type: value.type
                 })
                 setTasks([...copyTasks]);
-            })
-
-            dataValues.map((task) => {
-                if(!columns.find(column => column.column === task.type)) {
-                    const copyColumns = columns;
-                    copyColumns.push({column: task.type, showForm: false});
-                    setColumns([...copyColumns]);
-                }
             })
         }
     }
@@ -144,11 +153,30 @@ export const Column = () => {
         await axiosTasks.delete(`https://task-manager-project-66e0f-default-rtdb.firebaseio.com/tasks/${tasks[index!].id}.json`);
     }
 
+    const addColumnHandler = () => {
+        setShowNewColumn(!showNewColumn);
+    }
+
+    const addNewColumnHandler = async (e: any) => {
+        e.preventDefault();
+
+        await axiosTasks.post('columns.json', {
+            column: `${newColumn}`,
+            showForm: `false`,
+        })
+
+        const copyColumns = columns;
+        copyColumns.push({column: newColumn!, showForm: false});
+        setColumns([...copyColumns]);
+
+        setShowNewColumn(false);
+    }
+
     useEffect(() => {
         const copyTasks = tasks;
         copyTasks.splice(0, copyTasks.length);
         setTasks([...copyTasks]);
-        getTasks();
+        getData();
     }, []);
 
     return(
@@ -163,56 +191,71 @@ export const Column = () => {
             />
             <div className='columns'>
                 {columns.map((column, index) => (
-                    <ScrollArea 
-                        className='column'
-                        onDragOver={(e) => onDragOverHanlder(e)}
-                        onDragLeave={(e) => onDragLeaveHandler(e)}
-                        onDrop={(e) => onDropHandler(e, column.column)}
-                    >
-                        <div className="column-header">
-                            {column.column}
-                        </div>
-                        <div className="tasks">
-                            {
-                                tasks.map((task, index) => (
-                                    task.type === column.column
-                                    ? 
-                                        <ContextMenu key={Date.now() * index}>
-                                            <ContextMenuTrigger style={{width: `100%`}}>
-                                                <Badge
-                                                    draggable={true}
-                                                    className='task'
-                                                    key={index}
-                                                    onDragStart={() => onDragStartHandler(task)}
-                                                >
-                                                    <div dangerouslySetInnerHTML={{ __html: task.task}}/>
-                                                </Badge>
-                                            </ContextMenuTrigger>
-                                            <ContextMenuContent>
-                                                <ContextMenuItem onClick={() => onShowEditModalHandler(index)}>Edit</ContextMenuItem>
-                                                <ContextMenuItem onClick={() => onDeleteCardHanlder(index)}>Delete</ContextMenuItem>
-                                            </ContextMenuContent>
-                                        </ContextMenu>
-                                    :
-                                        <></>
-                                ))
-                            }
-                        </div>
-                        <form onSubmit={(e) => onCreateCardHandler(e, column)} className='add-card-form' style={{display: column.showForm ? 'block' : 'none'}}>
-                            <div className="input-container">
-                                <Input placeholder='Введите новое задание' className='newTask' onChange={(e) => setNewTask(e.target.value)} value={newTask}/>
-                            </div>
-                        </form>
-                        <div className="add-card">
-                            <Button onClick={() => toggleShowForm(index)} style={{display: column.showForm ? 'none' : 'flex'}}>
-                                <p>+</p>
-                                <p>Добавить карточку</p>
-                            </Button>
-                            <Button style={{display: column.showForm ? 'block' : 'none'}} onClick={(e) => onCreateCardHandler(e, column)}>Добавить</Button>
-                            <Button style={{display: column.showForm ? 'block' : 'none', fontSize: "20px"}} onClick={() => toggleShowForm(index)} className='edit-cancel'><IoIosClose /></Button>
+                    <ContextMenu>
+                        <ContextMenuTrigger>
+                            <ScrollArea
+                                key={index * Date.now()}
+                                className='column'
+                                onDragOver={(e) => onDragOverHanlder(e)}
+                                onDragLeave={(e) => onDragLeaveHandler(e)}
+                                onDrop={(e) => onDropHandler(e, column.column)}
+                            >
+                                <div className="column-header">
+                                    {column.column}
+                                </div>
+                                <div className="tasks">
+                                    {
+                                        tasks.map((task, index) => (
+                                            task.type === column.column
+                                            ? 
+                                                <ContextMenu key={Date.now() * index}>
+                                                    <ContextMenuTrigger style={{width: `100%`}}>
+                                                        <Badge
+                                                            draggable={true}
+                                                            className='task'
+                                                            key={index}
+                                                            onDragStart={() => onDragStartHandler(task)}
+                                                        >
+                                                            <div dangerouslySetInnerHTML={{ __html: task.task}}/>
+                                                        </Badge>
+                                                    </ContextMenuTrigger>
+                                                    <ContextMenuContent>
+                                                        <ContextMenuItem onClick={() => onShowEditModalHandler(index)}>Edit</ContextMenuItem>
+                                                        <ContextMenuItem onClick={() => onDeleteCardHanlder(index)}>Delete</ContextMenuItem>
+                                                    </ContextMenuContent>
+                                                </ContextMenu>
+                                            :
+                                                <></>
+                                        ))
+                                    }
+                                </div>
+                                <form onSubmit={(e) => onCreateCardHandler(e, column)} className='add-card-form' style={{display: column.showForm ? 'block' : 'none'}}>
+                                    <div className="input-container">
+                                        <Input placeholder='Введите новое задание' className='newTask' onChange={(e) => setNewTask(e.target.value)} value={newTask}/>
+                                    </div>
+                                </form>
+                                <div className="add-card">
+                                    <Button onClick={() => toggleShowForm(index)} style={{display: column.showForm ? 'none' : 'flex'}}>
+                                        <p>+ Добавить карточку</p>
+                                    </Button>
+                                    <Button style={{display: column.showForm ? 'block' : 'none'}} onClick={(e) => onCreateCardHandler(e, column)}>Добавить</Button>
+                                    <Button style={{display: column.showForm ? 'block' : 'none', fontSize: "20px"}} onClick={() => toggleShowForm(index)} className='edit-cancel'><IoIosClose /></Button>
+                                </div>
+                            </ScrollArea>
+                        </ContextMenuTrigger>
+                        <ContextMenuContent>
+                            <ContextMenuItem>Delete</ContextMenuItem>
+                        </ContextMenuContent>
+                    </ContextMenu>
+                ))}
+                <form onSubmit={addNewColumnHandler}>
+                    <ScrollArea className='column new-column' style={{display: showNewColumn ? 'flex' : 'none'}}>
+                        <Input placeholder='Введите название таблицы...' style={{marginTop: "10px"}} onChange={(e) => setNewColumn(e.target.value)}/>
+                        <div className='add-card confirm-new-column-btn'>
+                            <Button type='submit'>+ Добавить колонку</Button>
                         </div>
                     </ScrollArea>
-                ))}
+                </form>
             </div>
             <div className="alert">
                 <Alert style={{display: showAlert ? 'block' : 'none', borderColor: "#d9534f", color: '#d9534f'}}>
@@ -223,6 +266,7 @@ export const Column = () => {
                     </AlertDescription>
                 </Alert>
             </div>
+            <button className='add-column-btn' onClick={addColumnHandler}>+</button>
         </div>
     );
 }
